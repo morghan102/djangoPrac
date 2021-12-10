59a7acd28d264dceecc49f58b718efac950ff6ba
morghan102 <morghan102@gmail.com>
2021-12-10 14:13:58 -0800

    initial commit2

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/morghan/coding/practice/python/pyBackendWebDevwDjango/myapp/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ myproj/myapp/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index') #route, view, name
+    # empty '' means its the root, views is a file in this folder. index is the class or fun or ~
+]
