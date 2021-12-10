f9cef36bf9e1340ce673e8e6a16a085ccd9398ce
morghan102 <morghan102@gmail.com>
2021-12-10 15:00:56 -0800

    POST for more security, csrf_token

--- myproj/myapp/views.py
+++ myproj/myapp/views.py
@@ -12,6 +12,6 @@ def index(request):
     return render(request, 'index.html')# context) #last param is for sending in dynamic data
 
 def counter(request):
-    text = request.GET['text'] #its getting the textarea in index.html bc its name is text and storing the text in this var
+    text = request.POST['text'] #its getting the textarea in index.html bc its name is text and storing the text in this var. make sure the post/get matches w wats in whatever is sending to it
     num_words = len(text.split())
     return render(request, 'counter.html', {'num': num_words})
