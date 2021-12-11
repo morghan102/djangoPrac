6d470394a1907e7edaf6119c427337892affe333
morghan102 <morghan102@gmail.com>
2021-12-10 20:36:26 -0800

    user registration

--- myproj/myapp/urls.py
+++ myproj/myapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'), #route, view, name
     # empty '' means its the root, views is a file in this folder. index is the class or fun or ~
-    path('counter', views.counter, name='counter')
+    path('counter', views.counter, name='counter'),
+    path('register', views.register, name='register')
 ]
--- myproj/myapp/views.py
+++ myproj/myapp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from .models import Feature
 
 # Create your views here.
@@ -46,3 +48,30 @@ def counter(request):
     text = request.POST['text'] #its getting the textarea in index.html bc its name is text and storing the text in this var. make sure the post/get matches w wats in whatever is sending to it
     num_words = len(text.split())
     return render(request, 'counter.html', {'num': num_words})
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email already used')
+                return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username already in use')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+                return redirect('login')
+        else:
+            messages.info(request, 'Passwords different')
+            return redirect('register')
+    else:
+        return render(request, 'register.html')
+
+    #save to the db
+    return render(request, 'register.html')
